9480e2d7e33ddd68280686645480763d1ee1933c
KarthikeyaGundumogula <karthikeya200226@gmail.com>
2024-09-24 10:51:51 +0000

    testing

---
+++ indie-games/programs/indie-games/src/lib.rs
@@ -0,0 +1,23 @@
+use anchor_lang::prelude::*;
+
+pub mod errors;
+pub mod instructions;
+pub mod state;
+
+// use state::*;
+// use errors::*;
+use instructions::assets::*;
+
+declare_id!("5aDMDM66ULuQvrjFtG4SnJT23mzoRfYmT4AnJZkPKgoe");
+
+#[program]
+pub mod indie_games {
+     use super::*;
+
+    pub fn initialize_assets(
+        ctx: Context<InitializeAssetData>,
+        args: InitializeAssetDataArgs,
+    ) -> Result<()> {
+        intialize_asset_handler(ctx, args)
+    }
+}
