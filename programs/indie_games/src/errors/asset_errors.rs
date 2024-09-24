9ea535bdc0761ce0cff38af50a2c2a0aa1ce200e
KarthikeyaGundumogula <karthikeya200226@gmail.com>
2024-09-24 10:46:50 +0000

    initialized anchor with earlier programs

--- programs/indie_games/src/errors/asset_errors.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[error_code]
-pub enum AssetErrors {
-    #[msg("Arguments dodn't matched")]
-    InvalidArguments,
-    #[msg("Minting new token failed")]
-    MintFailed
-}
